5a1a5cfba1dfb213af34f0a962e477eb23f432fb
Neonhh <thenjhu@gmail.com>
2024-10-06 18:22:19 -0400

    Adds triangle rendering in OpenGL

--- OpenGL/Triangle/main.cpp
+++ OpenGL/Triangle/main.cpp
@@ -1,23 +1,35 @@
 #include <GL/glut.h>
-void displayMe(void)
-{
+
+void init() {
+    glClearColor(0.18, 0.12, 0.28, 1.0); // Set background color
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); // Set viewing volume
+}
+
+void display() {
+
     glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.5, 0.5, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0, 0.0, 0.0); // Red
+        glVertex2f(0.0, 0.87);
+        glColor3f(0.0, 1.0, 0.0); // Green
+        glVertex2f(-0.5, 0.0);
+        glColor3f(0.0, 0.0, 1.0); // Blue
+        glVertex2f(0.5, 0.0);
     glEnd();
+
     glFlush();
 }
-int main(int argc, char** argv)
-{
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(300, 300);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("SAMPLE TEST");
-    glutDisplayFunc(displayMe);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Colorful  Triangle");
+    init();
+    glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
