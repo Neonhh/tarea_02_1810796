3dac73676cef04f0ae32364a417a748fb0a71926
Neonhh <thenjhu@gmail.com>
2024-10-06 15:20:59 -0400

    Adds triangle in OpenGL

--- OpenGL/Triangle/main.cpp
+++ OpenGL/Triangle/main.cpp
@@ -4,13 +4,14 @@ void init() {
     glClearColor(0.18, 0.12, 0.28, 1.0); // Set background color
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); // Set viewing volume
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0); // Defines range that will be visible on the screen
 }
 
 void display() {
 
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT); // Loads the background color
 
+    //Defines the triangle
     glBegin(GL_TRIANGLES);
         glColor3f(1.0, 0.0, 0.0); // Red
         glVertex2f(0.0, 0.87);
@@ -18,17 +19,22 @@ void display() {
         glVertex2f(-0.5, 0.0);
         glColor3f(0.0, 0.0, 1.0); // Blue
         glVertex2f(0.5, 0.0);
-    glEnd();
+    glEnd(); //ends definition of the triagle
 
-    glFlush();
+    glFlush(); //forces to render the triangle
 }
 
 int main(int argc, char** argv) {
+
+    //Setting up OpenGL environment
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
-    glutCreateWindow("Colorful  Triangle");
+    glutCreateWindow("OpenGL  Triangle");
+
     init();
+
+    //defines display as the callback function to draw the window
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
---
+++ OpenGL/Triangle/readme.md
@@ -0,0 +1,2 @@
+g++.exe  -o bin/Debug/Triangle.exe obj/Debug/main.o  -lfreeglut -lglew32 -lopengl32 -o main.exe  
+Output file is bin/Debug/Triangle.exe with size 127.06 KB
